9a7ba8c3eafee582aca1b7e73697c4a437694b5d
shi3719 <shi3719@naver.com>
2024-12-02 01:02:17 +0900

    수정2

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,13 +5,39 @@ const footer = document.querySelector('#footer');
 const loadSections = () => {
     fetch('/assets/temp/header.html')
         .then(response => response.ok ? response.text() : Promise.reject('Header not found'))
-        .then(data => { header.innerHTML = data; })
+        .then(data => {
+            header.innerHTML = data;
+            afterHeaderLoad();
+        })
         .catch(error => console.error('Header load error:', error));
 
     fetch('/assets/temp/footer.html')
         .then(response => response.ok ? response.text() : Promise.reject('Footer not found'))
-        .then(data => { footer.innerHTML = data; })
+        .then(data => {
+            footer.innerHTML = data;
+        })
         .catch(error => console.error('Footer load error:', error));
 };
 
 loadSections();
+
+// header 불러온 이후
+const afterHeaderLoad = () => {
+    // 햄버거 메뉴 조절
+    const mobileMenuOpenBtn = document.querySelector('.mobile_menu_open');
+    const mobileMenuPopup = document.querySelector('.mobile_menu_popup');
+
+    mobileMenuOpenBtn.addEventListener('click', () => {
+        mobileMenuOpenBtn.classList.toggle('active');
+        mobileMenuPopup.classList.toggle('on');
+    })
+
+    // 모바일 메뉴 오픈 
+    const subMenuBtns = document.querySelectorAll('.sub_menu_btn');
+    subMenuBtns.forEach(subMenuBtn => {
+        subMenuBtn.addEventListener('click', () => {
+            subMenuBtn.parentNode.parentNode.classList.toggle('on');
+            subMenuBtn.parentNode.parentNode.classList.toggle('off');
+        })
+    })
+}
